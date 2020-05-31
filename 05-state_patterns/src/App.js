12b9ab90cf12bff6389bb4c55411cbbb55179c34
Kwabena Ampofo <kampof1@students.towson.edu>
2020-05-31 08:51:02 -0700

    completed state pattern basics

---
+++ 05-state_patterns/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ScoreKeeper from "./components/ScoreKeeper";
+
+class App extends Component {
+    render() {
+        return <ScoreKeeper />;
+    }
+}
+
+export default App;
---
+++ 05-state_patterns/src/components/ScoreKeeper.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class ScoreKeeper extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            score: 0,
+        };
+        this.singleKill = this.singleKill.bind(this);
+        this.tripleKill = this.tripleKill.bind(this);
+    }
+
+    singleKill(e) {
+        this.setState({ score: this.state.score + 1 });
+    }
+
+    tripleKill(e) {
+        this.setState(this.incrementScore);
+        this.setState(this.incrementScore);
+        this.setState(this.incrementScore);
+    }
+
+    incrementScore(curState) {
+        return { score: curState.score + 1 };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Score is {this.state.score}</h1>
+                <button onClick={this.singleKill}>Single Kill</button>
+                <button onClick={this.tripleKill}>Triple Kill</button>
+            </div>
+        );
+    }
+}
+
+export default ScoreKeeper;
